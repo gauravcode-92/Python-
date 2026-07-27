29180f40d5e2e86cf625a07b1acc2d65d6815b5c
gauravcode-92 <gauravwork873@gmail.com>
2026-07-27 22:21:10 +0530

    Create fruit_ripe.py

---
+++ fruit_ripe.py
@@ -0,0 +1,12 @@
+fruit="Banana"
+color="Brown"
+
+if fruit=="Banana":
+	if color=="Green":
+		print("Unripe")
+	elif color=="Yellow":
+		print("Ripe")
+	elif color=="Brown":
+		print("Overripe")
+else:
+	print("Enter only Banana fruit")

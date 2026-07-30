f0a48a40b52786592480071132f8647d269d464d
gauravcode-92 <gauravwork873@gmail.com>
2026-07-30 12:19:38 +0530

    Create petfood.py

---
+++ petfood.py
@@ -0,0 +1,14 @@
+species="dog"
+age=2
+if species=="dog":	
+	if age<2:
+		print(species +" junior food")
+	else:
+		print(species +" senior food")
+elif species=="cat":
+	if age>5:
+		print(species +" senior food")
+	else:
+		print(species +" junior food")
+
+	

aa0e97c793adb55b8062dde66a535f37e88a29cb
gauravcode-92 <gauravwork873@gmail.com>
2026-09-02 21:57:47 +0530

    Create count_tuple.py

---
+++ Python_learning/Lecture 3/Practice/count_tuple.py
@@ -0,0 +1,6 @@
+# Write a program to count the number of students with the "A" grade in the following tuple.
+
+tup=("C","D","A","A","B","B","A")
+cnt=tup.count("A")
+print("The number of students with the 'A' grade count is:",cnt)
+

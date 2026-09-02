be3ee8f553d2d3b87fb84b2e1acd9a424561f582
gauravcode-92 <gauravwork873@gmail.com>
2026-09-02 10:26:39 +0530

    Create even_odd.py

---
+++ Python_learning/Lecture 2/Practice/even_odd.py
@@ -0,0 +1,7 @@
+# Write a program to check if a number entered by the user is even or odd.
+num=int(input("Enter a number:"))
+
+if(num%2==0):
+    print(num,"is an even number.")
+else:
+    print(num,"is an odd number.")

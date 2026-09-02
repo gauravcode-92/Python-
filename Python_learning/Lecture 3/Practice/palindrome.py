020b9e3c23d9f03f67992809516190107e3dd6cd
gauravcode-92 <gauravwork873@gmail.com>
2026-09-02 21:56:57 +0530

    Create palindrome.py

---
+++ Python_learning/Lecture 3/Practice/palindrome.py
@@ -0,0 +1,11 @@
+list=["p","g","p","p"]
+list1=list.copy()
+print(list1)
+list1.reverse()
+
+
+if(list==list1):
+    print("This is a Palindrome list.")
+else:
+    print("This is not a Palidrome list.")
+
